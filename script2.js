d458d9f0d5d6b7b338ddcc705c16b904c59c0bf1
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-04-18 11:39:28 +0200

    har lavet opga 9, 10, 11, 14 og med ny json fil

--- script2.js
+++ script2.js
@@ -2,21 +2,26 @@
 
 window.addEventListener("load", start);
 
-const jsonData =
-  "https://my-first-project-708c7-default-rtdb.europe-west1.firebasedatabase.app";
+const jsonData2 =
+  "https://try-out-new-data-structure-default-rtdb.firebaseio.com/";
+
 
 async function start() {
-  console.log("start is starting...");
+  console.log("start2 is starting...");
 
   const posts = await getPosts();
   for (const post of posts) {
     showPost(post);
   }
-//   createPost("my title", "img", "hello world");
+  const users = await getUsers();
+   for (const user of users) {
+     showUser(user);
+   }
+  //   createPost("my title", "img", "hello world");
 }
 
 async function getPosts() {
-  const response = await fetch(`${jsonData}/posts.json`);
+  const response = await fetch(`${jsonData2}/posts.json`);
   const data = await response.json();
 
   console.log(data);
@@ -24,6 +29,14 @@ async function getPosts() {
   const posts = preparePostData(data);
   console.log(posts);
   return posts;
+}
+
+async function getUsers() {
+  const response = await fetch(`${jsonData2}/users.json`);
+  const data = await response.json();
+
+  const users = prepareUserData(data);
+  return users;
 }
 
 function preparePostData(dataObject) {
@@ -40,6 +53,20 @@ function preparePostData(dataObject) {
   return anArray;
 }
 
+function prepareUserData(dataObject) {
+  const anArray = [];
+
+  for (const key in dataObject) {
+    const user = dataObject[key];
+    user.id = key;
+    // console.log(post);
+    anArray.push(user);
+  }
+  console.log("Array: " + anArray);
+
+  return anArray;
+}
+
 function showPost(post) {
   document.querySelector("#grid-container").insertAdjacentHTML(
     "beforeend",
@@ -47,16 +74,32 @@ function showPost(post) {
 <article class="grid-item">
 <img src="${post.image}">
 <h2>${post.title}</h2>
+<p>${post.uid}</p>
 <p>${post.body}</p>
 </article>`
   );
 }
 
-async function createPost(title, image, body) {
+function showUser(user) {
+      document.querySelector("#grid-container").insertAdjacentHTML(
+        "beforeend",
+        /*HTML*/ `
+<article class="grid-item">
+<img src="${user.image}">
+<p>${user.name}</p>
+<h2>${user.title}</h2>
+<p>${user.phone}</p>
+<p>${user.mail}</p>
+</article>`
+      );
+}
+
+async function createPost(title, image, body, uid) {
   const newPost = {
     title: title,
     image: image,
     body: body,
+    uid: uid
   };
   console.log(newPost);
   const postAsJson = JSON.stringify(newPost);
